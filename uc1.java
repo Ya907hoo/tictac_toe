8af3bbc3e51329b8e39a9809e99b2b9dfbcddcb5
Nitya <soniralongway@gmail.com>
2026-05-06 07:28:41 +0530

    first usecase

---
+++ uc1.java
@@ -0,0 +1,31 @@
+public class uc1 {
+
+    static char[][] board = new char[3][3];
+
+    // Entry point of the program
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    // Initialize the board with '-'
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    // Print the board in grid format
+    static void printBoard() {
+        System.out.println("Tic-Tac-Toe Board:");
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println(); // Move to next line after each row
+        }
+    }
+}
